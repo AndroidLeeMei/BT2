39910d836c340dc27b58337f1d28d2f789486041
LeeMei <boubouian@yahoo.com.tw>
2017-12-26 12:44:10 +0800

    Initial commit

---
+++ app/src/main/java/com/example/auser/bt2/MainActivity_1.java
@@ -0,0 +1,13 @@
+package com.example.auser.bt2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity_1 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_1);
+    }
+}
